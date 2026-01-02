1b098b5945646ddf55ffda18870fc265b998371a
Manoj V Poojar <manojvpoojar@gmail.com>
2026-01-02 13:36:37 +0530

    Fix Render PostgreSQL SSL disconnect issue

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -93,9 +93,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(
         os.environ.get("DATABASE_URL"),
-        conn_max_age=600
+        conn_max_age=0,      # VERY IMPORTANT
+        ssl_require=True
     )
 }
+
 
 
 
