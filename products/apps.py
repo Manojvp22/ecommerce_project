24d1863bdef228a9c5670b3a580608732c6abb21
Manoj V Poojar <manojvpoojar@gmail.com>
2026-01-02 18:25:53 +0530

    Remove temporary admin bootstrap code

--- products/apps.py
+++ products/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
 
+
 class ProductsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'products'
-
-    def ready(self):
-        from django.contrib.auth import get_user_model
-        User = get_user_model()
-
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username="admin",
-                email="admin@example.com",
-                password="admin123"
-            )
